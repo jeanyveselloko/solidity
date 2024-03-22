17944de304d303726f43cc5b8ffb7de6db554a85
jeanyvesellokoea <jeanyvesellokopro.ea@gmail.com>
2024-03-22 10:38:35 +0100

    first

--- tp1.sol
+++ tp1.sol
@@ -20,13 +20,13 @@ contract VotingSystem {
 // ajout de candidat
     function addCandidate(uint _id, string memory _name) public {
         require(msg.sender == owner, "seul le proprietaire peut ajouter un candidat ");
-        require(candidates[_id].id == 0, "cet identifiant existe deja ");
+        require(candidates[_id].id == 0, "cet identifiant existe");
         candidates[_id] = Candidate(_id, _name, 0);
         candidateIds.push(_id);
     }
 //fonction vote qui est possible une seul fois par candidat
     function vote(uint _candidateId) public {
-        require(!voters[msg.sender], "Vous avez deja vote");
+        require(!voters[msg.sender], "Vous avez deja vote ");
         require(candidates[_candidateId].id != 0, "le candidat n'exister pas");
         candidates[_candidateId].voteCount++;
         voters[msg.sender] = true;
